dc070c521888a8929d2f67bd2fc715ad3541fc05
Todd Littlejohn <littlejohnjt@hotmail.com>
2020-04-16 20:59:10 -0400

    Fixes for issues: 14, 11, 10, 8, 6, 5

---
+++ FitnessLeaderBoard/Utilities.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FitnessLeaderBoard
+{
+    public static class Utilities
+    {
+        public static string Truncate(this string input, int numberOfCharacters)
+        {
+            var inputLength = input.Length;
+
+            if (inputLength <= numberOfCharacters)
+                return input;
+            else
+            {
+                var result = input.Substring(0, numberOfCharacters - 3) + "...";
+                return result;
+            }
+        }
+    }
+}
